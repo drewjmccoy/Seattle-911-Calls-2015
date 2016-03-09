0315c92b04c96b12de9ce5bd3266bf00330a788c
Brendan Long <xcbrendanlong@gmail.com>
2016-03-08 19:05:07 -0800

    fixed subplot and hover

--- Scripts/Crime_Types.R
+++ Scripts/Crime_Types.R
@@ -7,22 +7,31 @@ summarize(instances = n())
 state_data <- plot_ly(summary,
                          x = Event.Clearance.Group,
                          y = instances,
-                         name = "killed",
+                         name = "Crime",
                          type = "bar",
                          hoverinfo = "none"
                         )
 state_data
 traffic_calls <- filter(summary, Event.Clearance.Group == "TRAFFIC RELATED CALLS")
 traffic_calls$Event.Clearance.Description <- as.character(traffic_calls$Event.Clearance.Description)
-traffic_calls <- mutate(traffic_calls, most_instances = most_instances(data,traffic_calls$Event.Clearance.Description))
+traffic_calls <- mutate(traffic_calls, greatest_street = sapply(traffic_calls$Event.Clearance.Description,most_instances, data = data))
+traffic_data <- plot_ly(traffic_calls,
+                        x = Event.Clearance.Description,
+                        y = instances,
+                        name = "Traffic Violations",
+                        type = "bar",
+                        text = greatest_street,
+                        hoverinfo = "text"
+                        )
 
-
-most_instances <- function(data, description){
+most_instances <- function(description, data){
+  data$Event.Clearance.Description <- as.character(data$Event.Clearance.Description)
+  data$Hundred.Block.Location <- as.character(data$Hundred.Block.Location)
   street <- filter(data, Event.Clearance.Description == description) %>% 
     group_by(Hundred.Block.Location) %>% 
-    summarize(instances = n()) %>% 
-    filter(instances == max(instances)) %>% 
-    select(Hundred.Block.Location)
-  return(as.character(street$Hundred.Block.Location[1]))
+    summarize(instances = n()) %>%   
+    arrange(-instances)
+  value <- street$Hundred.Block.Location[1]
+  return(value)
 }
 bunk <- most_instances(data, "ABANDONED VEHICLE")
